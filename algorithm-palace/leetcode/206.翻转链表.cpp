00ca1e367fdbc471627b56c64555aa10e2889bf1
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-09 10:16:13 +0800

    Create 206.翻转链表.cpp

---
+++ algorithm-palace/leetcode/206.翻转链表.cpp
@@ -0,0 +1,25 @@
+// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* node = NULL;
+        ListNode* cur = head;
+        while (cur){
+            ListNode* dummyNode = cur->next;
+            cur->next = node;
+            node = cur;
+            cur = dummyNode;
+        }
+        return node;
+    }
+};
