2271824ad278cea9f39d2d93cba0b36711170fa9
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-09 10:20:49 +0800

    Update 1.四数之和.cpp

--- algorithm-palace/leetcode/1.四数之和.cpp
+++ algorithm-palace/leetcode/1.四数之和.cpp
@@ -1,2 +1,20 @@
-class Solution{
-}
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+// 你可以按任意顺序返回答案。
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++){
+            if (hash.count(target - nums[i])){
+                result.push_back(hash[target - nums[i]]);
+                result.push_back(i);
+            }
+            hash[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
