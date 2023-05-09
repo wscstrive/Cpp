4e06dad0e786e7372832c7d404ea9545ba5d0466
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-09 10:11:43 +0800

    Create 704.二分查找.cpp

---
+++ algorithm-palace/leetcode/704.二分查找.cpp
@@ -0,0 +1,21 @@
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right){
+            int mid = (left + right) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
