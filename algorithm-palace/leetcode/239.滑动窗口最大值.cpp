22f05d5489ef2fd47275a08647179c7afb408dff
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-11 09:58:31 +0800

    Create 239.滑动窗口最大值.cpp

---
+++ algorithm-palace/leetcode/239.滑动窗口最大值.cpp
@@ -0,0 +1,29 @@
+// 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+// 返回 滑动窗口中的最大值 。
+// 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+// 输出：[3,3,5,5,6,7]
+// 解释：
+// 滑动窗口的位置                   最大值
+// ---------------                ----
+// [1  3  -1] -3  5  3  6  7       3
+//  1 [3  -1  -3] 5  3  6  7       3
+//  1  3 [-1  -3  5] 3  6  7       5
+//  1  3  -1 [-3  5  3] 6  7       5
+//  1  3  -1  -3 [5  3  6] 7       6
+//  1  3  -1  -3  5 [3  6  7]      7
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> result;
+        int len = nums.size();
+        for (int i = 0; i < len; i++){
+            if (!dq.empty() && dq.front() < i - k + 1) dq.pop_front();
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) result.push_back(nums[dq.front()]);
+        }
+        return result;
+    }
+};
