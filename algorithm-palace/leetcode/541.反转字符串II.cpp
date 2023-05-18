41db22b0aaeb3d39c7758d0b43e0787b9d0778ae
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-18 10:49:45 +0800

    Create 541.反转字符串II.cpp

---
+++ algorithm-palace/leetcode/541.反转字符串II.cpp
@@ -0,0 +1,27 @@
+// 给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+// 如果剩余字符少于 k 个，则将剩余字符全部反转。
+// 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+
+// 示例 1：
+
+// 输入：s = "abcdefg", k = 2
+// 输出："bacdfeg"
+
+class Solution {
+public:
+    void reverse(string& s, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            swap(s[i], s[j]);
+        }
+    }
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            if (i + k <= s.size()){
+                reverse(s, i, i + k - 1);
+                continue;
+            }
+            reverse(s, i, s.size() - 1);      
+        }
+        return s;
+    }
+};
