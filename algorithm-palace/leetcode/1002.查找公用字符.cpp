95bf5620b01a3bc709dfc593edb62a807acfa62e
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-22 09:58:27 +0800

    Update 1002.查找公用字符.cpp

--- algorithm-palace/leetcode/1002.查找公用字符.cpp
+++ algorithm-palace/leetcode/1002.查找公用字符.cpp
@@ -1,4 +1,8 @@
 // 给你一个字符串数组 words ，请你找出所有在 words 的每个字符串中都出现的共用字符（ 包括重复字符），并以数组形式返回。你可以按 任意顺序 返回答案。
+// 示例 1：
+
+// 输入：words = ["bella","label","roller"]
+// 输出：["e","l","l"]
 
 class Solution {
 public:
