c63741c9e6d9ecdbf35b697f8fa32eff40cf3fad
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-28 12:40:38 +0800

    Update 977.有序数组的平方.cpp

--- algorithm-palace/leetcode/977.有序数组的平方.cpp
+++ algorithm-palace/leetcode/977.有序数组的平方.cpp
@@ -1,13 +1,31 @@
 // 给你一个按 非递减顺序 排序的整数数组 nums，返回每个数字的平方 组成的新数组，要求也按非递减顺序排序。
 
-class Solution {
-public:
-    
-    vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i){
-            nums[i] = nums[i] * nums[i];
-        }
-        sort(nums.begin(), nums.end());
-        return nums;
-    }
-};
+// c++
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r, k = 0, len(nums) - 1, len(nums) - 1
+        result = [float('inf')] * len(nums)
+        while l <= r:
+            if nums[l] ** 2 < nums[r] ** 2:
+                result[k] = nums[r] ** 2
+                r -= 1
+            else:
+                result[k] = nums[l] ** 2
+                l += 1
+            k -= 1
+        return result
+
+// python
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r, k = 0, len(nums) - 1, len(nums) - 1
+        result = [float('inf')] * len(nums)
+        while l <= r:
+            if nums[l] ** 2 < nums[r] ** 2:
+                result[k] = nums[r] ** 2
+                r -= 1
+            else:
+                result[k] = nums[l] ** 2
+                l += 1
+            k -= 1
+        return result
