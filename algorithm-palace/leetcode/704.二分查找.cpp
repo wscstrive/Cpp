42d012194f0f7d7e20e846f3cc598c99e0f1042e
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-28 10:50:35 +0800

    Update 704.二分查找.cpp

--- algorithm-palace/leetcode/704.二分查找.cpp
+++ algorithm-palace/leetcode/704.二分查找.cpp
@@ -1,5 +1,6 @@
 // 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 
+// c++
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -19,3 +20,17 @@ public:
         return -1;
     }
 };
+
+// python3
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = left + 1
+            else:
+                right = right - 1
+        return -1
