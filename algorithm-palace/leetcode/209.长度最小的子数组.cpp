92871e59a9a7613403acb07d29fe093ce2d24d77
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-05-28 12:41:32 +0800

    Update 209.长度最小的子数组.cpp

--- algorithm-palace/leetcode/209.长度最小的子数组.cpp
+++ algorithm-palace/leetcode/209.长度最小的子数组.cpp
@@ -1,19 +1,40 @@
 // 给定一个含有 n 个正整数的数组和一个正整数 target 。
 // 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
 
+// c++
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int dist = INT32_MAX;
+        int result = INT32_MAX;
         int l = 0;
         int sum = 0;
-        for (int r = 0; r < nums.size(); r++){
+        for (int r = 0; r < nums.size(); r++) {
             sum += nums[r];
-            while (sum >= target){
-                dist = dist > (r - l + 1) ? (r - l + 1) : dist;
+            while (sum >= target) {
+                result = r - l + 1 < result ? r - l + 1 : result;
                 sum -= nums[l++];
             }
         }
-        return dist == INT32_MAX ? 0 : dist;
+        return result == INT32_MAX ? 0 : result;
     }
 };
+
+// python
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        sum = 0
+        result = inf
+        for r in range(len(nums)):
+            sum += nums[r]
+            while sum >= target:
+                if r - l + 1 < result:
+                    result = r - l + 1
+                else:
+                    result = result
+                sum -= nums[l]
+                l += 1
+        if result == inf:
+            return 0
+        else:
+            return result
