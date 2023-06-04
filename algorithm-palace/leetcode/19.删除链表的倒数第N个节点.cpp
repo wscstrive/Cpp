2a96fc2366c8e420cfbdb755b480ec60da7ac002
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-06-04 14:53:17 +0800

    Update 19.删除链表的倒数第N个节点.cpp

--- algorithm-palace/leetcode/19.删除链表的倒数第N个节点.cpp
+++ algorithm-palace/leetcode/19.删除链表的倒数第N个节点.cpp
@@ -1,5 +1,7 @@
 // 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 
+
+// cpp
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -30,3 +32,22 @@ public:
 
     }
 };
+
+// python
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummyNode = ListNode(0, head)
+        left = dummyNode
+        right = dummyNode
+        for _ in range(n):
+            right = right.next
+        while (right.next != None):
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummyNode.next
