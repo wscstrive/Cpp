933ee5c635b3b48ec39c4e186516f52b81c3d986
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-06-05 12:16:53 +0800

    Update 142.环形链表.cpp

--- algorithm-palace/leetcode/142.环形链表.cpp
+++ algorithm-palace/leetcode/142.环形链表.cpp
@@ -2,6 +2,7 @@
 // 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
 // 不允许修改 链表。
 
+// cpp
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -30,3 +31,25 @@ public:
         return NULL;
     }
 };
+
+// python
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return fast
+        return None
