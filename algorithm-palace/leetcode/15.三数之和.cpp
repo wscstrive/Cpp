eb6d65e10337fd145f7b913d3df15cb20acf577d
ellery3 <101634608+ellery3@users.noreply.github.com>
2023-06-05 12:40:09 +0800

    Update 15.三数之和.cpp

--- algorithm-palace/leetcode/15.三数之和.cpp
+++ algorithm-palace/leetcode/15.三数之和.cpp
@@ -2,6 +2,7 @@
 // 你返回所有和为 0 且不重复的三元组。
 // 注意：答案中不可以包含重复的三元组。
 
+// cpp
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -40,3 +41,30 @@ public:
         return result;
     }
 };
+
+// python
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                return result
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                if nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return result
